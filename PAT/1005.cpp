4312125a2292581bae428e28450843b7bfbfbee0
秉亮 <bl_indie@qq.com>
2014-09-26 00:51:22 +0800

    2014/9/26

--- PAT/1005.cpp
+++ PAT/1005.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-bool COVERED[100]={false};
-int NUM[100]={0};
+bool COVERED[100] = { false };
+vector<int>NUM;
 
 int Callatz(int n)
 {
-	if (n == 1)
-	{return 0;}
-	else if (n%2==0)
-	{n/=2;COVERED[n]=true;return Callatz(n);}
-	else
-	{
-		n = (3*n+1)/2;
-		COVERED[n]=true;
+	if (n == 1)return 0;
+	else if (n % 2 == 0){
+		n /= 2;
+		if(n<=100)COVERED[n] = true;
+		return Callatz(n);
+	}
+	else{
+		n = (3 * n + 1) / 2;
+		if (n <= 100)COVERED[n] = true;
 		return Callatz(n);
 	}
 }
@@ -21,29 +24,32 @@ int Callatz(int n)
 int main()
 {
 	int N;
-	cin>>N;
-	for (int i=0;i!=N;i++)
+	cin >> N;
+	for (int i = 0; i != N; i++)
 	{
 		int K;
-		cin>>K;
-		NUM[i]=K;
-		if (COVERED[K]==true)
-		{continue;}
-		else
-		{Callatz(K);}		
+		cin >> K;
+		NUM.push_back(K);
+		if (K<=100&&COVERED[K] == true){
+			continue;
+		}
+		else Callatz(K);
 	}
-	bool find_the_lagest=false;
-	for (int i=N-1;i>=0;i--)
+	sort(NUM.begin(), NUM.end());
+	bool find_the_lagest = false;
+	for (int i = N - 1; i >= 0; i--)
 	{
-		if (find_the_lagest==false)
+		if (find_the_lagest == false)
 		{
-			if (COVERED[NUM[i]]==false)
-			{cout<<NUM[i];find_the_lagest=true;}
+			if (COVERED[NUM[i]] == false)
+			{
+				cout << NUM[i]; find_the_lagest = true;
+			}
 		}
 		else
 		{
-			if (COVERED[NUM[i]]==false)
-			cout<<" "<<NUM[i];
+			if (COVERED[NUM[i]] == false)
+				cout << " " << NUM[i];
 		}
 	}
 	system("pause");
--- PAT/1007.cpp
+++ PAT/1007.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include<iostream>
+#include<cmath>
 using namespace std;
 
 bool isOdd(int n)
@@ -6,7 +7,7 @@ bool isOdd(int n)
 	if(n<=1||n>=1E5)
 	{return false;}
 
-	for(int i=2;i!=n/2+1;i++)
+	for(int i=2;i<=sqrt(n);i++)
 	{if (n%i==0) return false;}
 
 	return true;
