4397ccf401faa1830ed9a75164f16374477d003c
秉亮 <bl_indie@qq.com>
2015-08-10 18:12:02 +0800

    增加gitignore，修改1013

--- PAT/1013.cpp
+++ PAT/1013.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool InsertPrime(int N);
-bool isPrime(vector<int> Primes,int Number);
+bool isPrime(int number);
 
-vector<int> Primes;
-int main()
-{
-	int M,N;
+int main() {
+	int M, N;
 
-	cin>>M>>N;//M&N<=10^4
-	if (M>N||N>1E4||M<=0)
-	{return 1;}
+	cin >> M >> N;
 
-	for(int i=0;i!=N;i++)
-	{InsertPrime(i);}
-
-	
-	cout<<Primes[M-1];
-	int circle=1;
-	for(int i=M;i<=N-1;i++,circle++)
-	{
-		if (circle%10!=0)
-		{cout<<' ';}
-		else
-		{cout<<endl;}
-		cout<<Primes[i];
+	int count = 0;
+	int row = 0;
+	int number = 2;
+	while (count<N) {
+		if (isPrime(number)) {
+			count++;
+			if (count >= M) {
+				row++;
+				if (count == N) {
+					cout << number;
+				}
+				else {
+					if (row % 10 != 0) {
+						cout << number << ' ';
+					}
+					else {
+						cout << number << endl;
+					}
+				}
+			}
+		}
+		number++;
+		
 	}
-
 	system("pause");
 	return 0;
 }
 
-bool InsertPrime(int N)
-{
-	if(N==0){Primes.push_back(2);return true;}
-	int max_Prime=*(Primes.end()-1);
-	while(true)
-	{
-		max_Prime++;
-		if(isPrime(Primes,max_Prime))
-		{Primes.push_back(max_Prime);return true;}		
+bool isPrime(int number) {
+	for (int i = 2; i*i <= number; i++) {
+		if (number%i == 0) {
+			return false;
+		}
 	}
-}
-
-bool isPrime(vector<int> Primes,int Number)
-{
-	for(int i=0;i!=Primes.size();i++)
-	{if(Number%Primes[i]==0)return false;}
 	return true;
 }
